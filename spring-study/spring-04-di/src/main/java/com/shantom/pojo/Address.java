a6bbc8c3b8c2c2e36fe3024385f3f8d8a62b1cb2
Shantom <salpha1345@gmail.com>
2020-11-26 21:49:18 +0800

    04-di

---
+++ spring-study/spring-04-di/src/main/java/com/shantom/pojo/Address.java
@@ -0,0 +1,13 @@
+package com.shantom.pojo;
+
+public class Address {
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
