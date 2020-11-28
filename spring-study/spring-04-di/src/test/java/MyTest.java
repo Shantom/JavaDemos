2a91acea4e994b53562aaa4fc7d23f1a8cf91ac9
Shantom <salpha1345@gmail.com>
2020-11-28 11:36:38 +0800

    gitignore p c命名

--- spring-study/spring-04-di/src/test/java/MyTest.java
+++ spring-study/spring-04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.shantom.pojo.Student;
+import com.shantom.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,5 +9,14 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student) context.getBean("student");
         System.out.println(student.getName());
+    }
+
+    @Test
+    public void test1(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+        User user2 = context.getBean("user2", User.class);
+        System.out.println(user2);
     }
 }
