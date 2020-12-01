94f64d1ed33935f00612423deed65d95221bdbe4
ningwei.shi <1044950475@qq.com>
2020-12-01 20:35:10 +0800

    动态代理

--- spring-study/spring-08-proxy/src/main/java/com/shantom/demoDynamic/UserInvocationHandler.java
+++ spring-study/spring-08-proxy/src/main/java/com/shantom/demoDynamic/UserInvocationHandler.java
@@ -3,9 +3,29 @@ package com.shantom.demoDynamic;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class UserInvocationHandler implements InvocationHandler {
+    private Object target;
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public Object getTarget() {
+        return target;
+    }
+
+    public Object getProxy(){
+        return Proxy.newProxyInstance(getClass().getClassLoader(), target.getClass().getInterfaces(),this);
+    }
+
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return method.invoke(proxy, args);
+        log(method.getName());
+        return method.invoke(target, args);
+    }
+
+    public void log(String msg){
+        System.out.println("执行了"+msg+"方法");
     }
 }
--- spring-study/spring-08-proxy/src/test/java/MyTest.java
+++ spring-study/spring-08-proxy/src/test/java/MyTest.java
@@ -1,6 +1,10 @@
+import com.shantom.demoDynamic.UserInvocationHandler;
 import com.shantom.demoStatic.UserProxy;
+import com.shantom.demoStatic.UserService;
 import com.shantom.demoStatic.UserServiceImpl;
 import org.junit.Test;
+
+import java.lang.reflect.Proxy;
 
 public class MyTest {
 
@@ -14,6 +18,13 @@ public class MyTest {
 
     @Test
     public void testDynamic(){
+        //真实角色
+        UserService userService = new UserServiceImpl();
 
+        UserInvocationHandler pih = new UserInvocationHandler();
+        pih.setTarget(userService);
+
+        UserService proxy = (UserService) pih.getProxy();
+        proxy.add();
     }
 }
