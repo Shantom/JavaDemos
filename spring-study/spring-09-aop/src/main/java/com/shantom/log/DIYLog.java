5aac62c7274c98f009758591fbc1213ad80e686a
ningwei.shi <1044950475@qq.com>
2020-12-01 15:58:06 +0800

    aop-diy

---
+++ spring-study/spring-09-aop/src/main/java/com/shantom/log/DIYLog.java
@@ -0,0 +1,10 @@
+package com.shantom.log;
+
+public class DIYLog {
+    public void before(){
+        System.out.println("before invoking");
+    }
+    public void after(){
+        System.out.println("after invoking");
+    }
+}
